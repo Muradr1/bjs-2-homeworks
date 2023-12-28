bea897abfecb894bdc0503a5297a2dd986959064
Murad Rzaev <rzaev_murad@bk.ru>
2023-12-28 21:04:04 +0300

    Все задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let d = (b ** 2) - 4 * a * c;
   let arr = [];
+
+  if(d < 0) {
+    arr = [];
+  }else if(d === 0) {
+    arr.push((-b / (2 * a)));
+  }else if(d > 0) {
+    arr.push((-b + Math.sqrt(d) ) / (2 * a)),
+    arr.push((-b - Math.sqrt(d) ) / (2 * a));
+  };
   
-  return arr;
-}
+    return arr;
+};
+solveEquation(1, 5, 4);
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let n = countMonths;
+  let payment = S * (P + (P / (((1 + P) ** n) - 1)));
+  let result = payment * countMonths;
   
-}
+  result = (result).toFixed(2);
+
+    return (Number (result));
+};
+calculateTotalMortgage(10, 0, 50000, 12);
